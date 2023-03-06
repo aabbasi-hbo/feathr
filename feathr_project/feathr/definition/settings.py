0dd42d10fe146e104f9afff035118ab007328058
aabbasi-hbo <92401544+aabbasi-hbo@users.noreply.github.com>
2023-03-05 19:21:45 -0800

    Update settings.py

--- feathr_project/feathr/definition/settings.py
+++ feathr_project/feathr/definition/settings.py
@@ -34,7 +34,7 @@ class ObservationSettings(HoconConvertible):
                         timestampColumn: {
                             def: "{{setting.event_timestamp_column}}"
                             format: "{{setting.timestamp_format}}"
-                        },
+                        }
                         simulateTimeDelay: 1d
                     }
                 }
